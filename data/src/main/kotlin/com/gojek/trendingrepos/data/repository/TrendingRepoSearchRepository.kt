5824313f92d5a3230f7092acd4a63ccdeb6d590a
Anoop M <anoopmaddasseri@gmail.com>
2020-02-16 17:48:08 +0530

    [GETR-1001] Data layer repository query

--- data/src/main/kotlin/com/gojek/trendingrepos/data/repository/TrendingRepoSearchRepository.kt
+++ data/src/main/kotlin/com/gojek/trendingrepos/data/repository/TrendingRepoSearchRepository.kt
@@ -1,7 +1,11 @@
 package com.gojek.trendingrepos.data.repository
 
-import com.gojek.trendingrepos.data.source.TrendingRepoSearchDataSource
+import com.gojek.trendingrepos.domain.models.TrendingRepo
 import com.gojek.trendingrepos.domain.repository.ITrendingRepoSearchRepository
+import com.gojek.trendingrepos.data.mappers.toDomain
+import com.gojek.trendingrepos.data.source.TrendingRepoSearchDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 /**
@@ -10,5 +14,10 @@ import javax.inject.Inject
 class TrendingRepoSearchRepository @Inject constructor(
     private val trendingRepoSearchDataSource: TrendingRepoSearchDataSource
 ) : ITrendingRepoSearchRepository {
+
+    override suspend fun searchTrendingRepositories(): Flow<List<TrendingRepo>> {
+        return trendingRepoSearchDataSource.query()
+            .map { it.map { results -> results.toDomain() } }
+    }
 
 }
--- data/src/main/kotlin/com/gojek/trendingrepos/data/source/TrendingRepoSearchDataSource.kt
+++ data/src/main/kotlin/com/gojek/trendingrepos/data/source/TrendingRepoSearchDataSource.kt
@@ -1,8 +1,25 @@
 package com.gojek.trendingrepos.data.source
 
 import com.gojek.trendingrepos.data.api.TrendingRepoApiService
+import com.gojek.trendingrepos.data.mappers.toEntity
+import com.gojek.trendingrepos.data.models.entity.TrendingRepoEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class TrendingRepoSearchDataSource @Inject constructor(private val apiService: TrendingRepoApiService) {
+
+    /**
+     * Takes in [params] to be used for the search
+     * @return list of search results
+     */
+    suspend fun query(): Flow<List<TrendingRepoEntity>> {
+        val searchResponse = apiService.searchTrendingRepositories()
+        val searchDataModels = mutableListOf<TrendingRepoEntity>()
+        for (searchResult in searchResponse) {
+            searchDataModels.add(searchResult.toEntity())
+        }
+        return flow { emit(searchDataModels) }
+    }
 
 }
