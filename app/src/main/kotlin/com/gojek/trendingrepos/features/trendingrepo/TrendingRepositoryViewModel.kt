edec33563491da3e9294378cd08b9bc3f95ab7c4
Anoop M <anoopmaddasseri@gmail.com>
2020-02-16 19:19:50 +0530

    [GETR-1001] Integrate trending repo fetch github web api

--- app/src/main/kotlin/com/gojek/trendingrepos/features/trendingrepo/TrendingRepositoryViewModel.kt
+++ app/src/main/kotlin/com/gojek/trendingrepos/features/trendingrepo/TrendingRepositoryViewModel.kt
@@ -3,9 +3,15 @@ package com.gojek.trendingrepos.features.trendingrepo
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.gojek.trendingrepos.commons.Loading
+import com.gojek.trendingrepos.commons.Success
 import com.gojek.trendingrepos.commons.UiStateViewModel
 import com.gojek.trendingrepos.domain.usecases.TrendingRepositoryUseCase
+import com.gojek.trendingrepos.mappers.toPresentation
 import com.gojek.trendingrepos.models.TrendingRepositoryUiModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class TrendingRepositoryViewModel @Inject constructor(
@@ -18,6 +24,15 @@ class TrendingRepositoryViewModel @Inject constructor(
     private var _searchResultsTrendingRepository: MutableLiveData<List<TrendingRepositoryUiModel>> =
         MutableLiveData()
 
+    fun executeTrendingRepositorySearch() {
+        _uiState.value = Loading
+        viewModelScope.launch(handler) {
+            trendingRepositoryUseCase().collect { results ->
+                _searchResultsTrendingRepository.value = results.map { it.toPresentation() }
+            }
+            _uiState.value = Success
+        }
+    }
 
     override fun onDestroy(lifecycleOwner: LifecycleOwner) {
     }
