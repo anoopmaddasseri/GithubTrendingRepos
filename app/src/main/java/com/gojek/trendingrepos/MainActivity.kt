a29c19bf5af5f365b286b908c5fcc55ddbc418ad
Anoop M <anoopmaddasseri@gmail.com>
2020-02-16 06:02:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/gojek/trendingrepos/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gojek.trendingrepos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gojek.trendingrepos.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
